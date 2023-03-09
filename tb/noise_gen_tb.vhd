12b6f0dbeb6c2dbd83823719f37a604e53bfc006
omerorkn <76444663+omerorkn@users.noreply.github.com>
2023-03-09 09:31:26 +0300

    indentation issue fixed

--- tb/noise_gen_tb.vhd
+++ tb/noise_gen_tb.vhd
@@ -11,7 +11,7 @@
 -- File history:
 -- 00   	: 02.02.2023 : File created.
 -- 01 		: 06.02.2023 : Comments added to code.
--- 02		: 08.02.2023 : "textio" library used to write output data to a text file.
+-- 02		  : 08.02.2023 : "textio" library used to write output data to a text file.
 --
 -- Note : If you would like to write all the noise data for create a line or histogram on excel, you can uncomment between line 94 - line 115 and
 -- 		  you can use textio library for create an output text file. 
@@ -25,10 +25,10 @@ use STD.TEXTIO.all;
 
 entity noise_gen_tb is
     generic (
-        DATA_WIDTH 	: integer := 5;                                                                         -- noise out bit width
-        CLK_FREQ 	: integer := 100_000_000;                                                               -- system clock frequency
-        SHIFT_FREQ 	: integer := 100_000_000;                                                               -- shifting frequency
-        FILE_NAME       : string  := "./log_file.dat"                                                           -- file name for output text file
+        DATA_WIDTH 	: integer := 5;                                                         -- noise out bit width
+        CLK_FREQ 	  : integer := 100_000_000;                                               -- system clock frequency
+        SHIFT_FREQ 	: integer := 100_000_000;                                               -- shifting frequency
+        FILE_NAME   : string  := "./log_file.dat"                                           -- file name for output text file
     );
 end noise_gen_tb;
 
@@ -36,28 +36,28 @@ architecture testbench of noise_gen_tb is
 
     component noise_gen is
         generic (
-            DATA_WIDTH  : integer := 5;							                        -- noise out bit width
-            CLK_FREQ	: integer := 100_000_000;                                                               -- system clock frequency
-            SHIFT_FREQ	: integer := 100	               				                        -- shifting frequency
+            DATA_WIDTH  : integer := 5;							                                        -- noise out bit width
+            CLK_FREQ	  : integer := 100_000_000;                                           -- system clock frequency
+            SHIFT_FREQ	: integer := 100	               				                            -- shifting frequency
         );						
         port (						
         -- Input Ports						
-            clk         : in std_logic;						                                -- system clock input
-            rst_n 	: in std_logic;					                                        -- active low reset	
-            en 		: in std_logic;						                                -- enable trigger
+            clk         : in std_logic;						                                          -- system clock input
+            rst_n 	    : in std_logic;					                                            -- active low reset	
+            en 		      : in std_logic;						                                          -- enable trigger
                     
         -- Output Ports		
-            noise_out 	: out std_logic_vector(DATA_WIDTH - 1 downto 0)		                                -- noise out port
+            noise_out 	: out std_logic_vector(DATA_WIDTH - 1 downto 0)		                  -- noise out port
         );
     end component;
 
-    constant CLK_PERIOD         : time := 1000 ms / CLK_FREQ;                                                   -- constant for clock generation
+    constant CLK_PERIOD     : time := 1000 ms / CLK_FREQ;                                   -- constant for clock generation
     
-    signal clk 	                : std_logic := '1';								-- test signal for system clock source
-    signal rst_n 	        : std_logic := '1';								-- test signal for active low reset
-    signal en 		        : std_logic := '0';								-- test signal for module enable trigger
-    signal noise_out 	        : std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');     	        -- test signal for generated noise out
-    signal binary_counter 	: std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');	                -- binary counter for observing maximal-length LFSR
+    signal clk 	            : std_logic := '1';								                              -- test signal for system clock source
+    signal rst_n 	          : std_logic := '1';								                              -- test signal for active low reset
+    signal en 		          : std_logic := '0';								                              -- test signal for module enable trigger
+    signal noise_out 	      : std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0'); -- test signal for generated noise out
+    signal binary_counter 	: std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');	-- binary counter for observing maximal-length LFSR
 
 begin
 
@@ -117,14 +117,14 @@ begin
     UUT : noise_gen
     generic map (
         DATA_WIDTH 	=> DATA_WIDTH,
-        CLK_FREQ 	=> CLK_FREQ,
+        CLK_FREQ 	  => CLK_FREQ,
         SHIFT_FREQ 	=> SHIFT_FREQ
     )
     port map(
-        clk 		=> clk,
-        rst_n 		=> rst_n,
-        en 		=> en,
-        noise_out 	=> noise_out
+        clk 		    => clk,
+        rst_n 		  => rst_n,
+        en 		      => en,
+        noise_out   => noise_out
     );
     
 end testbench;
